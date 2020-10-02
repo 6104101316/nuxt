3cdc348a08e5a8d37acdf8d3427730a2dad6cb2a
unknown <chandetchanatip@gmail.com>
2020-10-02 23:44:31 +0700

    nnnn

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col />
+      <v-col>
+        <v-img
+          src="http://clipart-library.com/images_k/apple-logo-png-transparent-background/apple-logo-png-transparent-background-8.png"
+          max-width="350"
+        />
+      </v-col>
+      <v-col />
+    </v-row>
+    <br>
+    <br>
+    <br>
+    <v-rov align="center" justify="center">
+      <h1>Wellcome to iTunes's Search</h1>
+    </v-rov>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.theme--dark.v-application {
+    background-image: url('https://cdn.pixabay.com/photo/2017/01/18/17/14/girl-1990347_1280.jpg');
+    background-attachment: fixed;
+    background-position: 100% 100%;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
---
+++ pages/product/_id.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-row>
+      <v-col md="4" />
+      <v-col md="4" align="center" justify="center">
+        <h1>{{ id.trackName }}</h1>
+        <br>
+        <v-img :src="id.artworkUrl100" max-height="300" max-width="300" />
+      </v-col>
+      <v-col md="4" />
+    </v-row>
+    <v-col align="center" justify="center">
+      <h3>{{ id.artistName }}</h3>
+      <h3>CollectionName : {{ id.collectionName }}</h3>
+      <br>
+      <audio controls>
+        <source :src="id.previewUrl" type="audio/mpeg">
+      </audio>
+      <a
+        style="text-decoration: none;"
+        target="_blank"
+        :href="id.trackViewUrl"
+      />
+    </v-col>
+
+    <div class="text-center">
+      <v-btn :href="id.trackViewUrl">
+        GO SOME WHERE
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      id: this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style></style>
